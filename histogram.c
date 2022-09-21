a74cd31a89edfdb9b563a0fd34a5f5190bb4a9f7
Ghilas KASMI <73539312+bapn3454@users.noreply.github.com>
2022-09-21 15:12:16 +0200

    added edge pixel counter, convolution, gradient norm, binarization

--- histogram.c
+++ histogram.c
@@ -6,6 +6,18 @@
 #include "nrarith.h"
 #include "nralloc.h"
 
+    const int horizontal_gradient[3][3] = {
+            {-1,0,1},
+            {-2,0,2},
+            {-1,0,1}
+    };
+
+    const int vertical_gradient[3][3] = {
+            {-1,-2,-1},
+            {0,0,0},
+            {1,2,1}
+    };
+
 void histogramme(byte **img, long nrh, long nch, int *histogramme, FILE *file) {
 
     //initialise l'histogramme
@@ -83,6 +95,84 @@ double bhattacharyyaDistance(int* histogramme1,int* histogramme2){
     return distance;
 }
 
+byte** binarization(byte** orig, byte threshold, int nrl, int nrh, int ncl, int nch) {
+    int i = 0, j = 0;
+    byte **transformed = bmatrix(nrl, nrh, ncl, nch);
+
+    for(i = 0 ; i < nrh ; i++) {
+        for (j = 0 ; j < nch ; j++) {
+            if(orig[i][j] < threshold) {
+                transformed[i][j] = (byte) 0;
+            }
+            else {
+                transformed[i][j] = (byte) 255;
+            }
+        }
+    }
+
+    return transformed;
+}
+
+int edgePixels(byte** gradient, int nrl, int nrh, int ncl, int nch) {
+    int sum = 0, i = 0, j = 0;
+    for(i=nrl+1 ; i<nrh ; i++) {
+        for(j=ncl+1 ; j<nch ; j++) {
+            sum += gradient[i][j];
+        }
+    }
+    return sum / 255;
+}
+
+byte** gradientNorm(byte** gradX, byte**gradY, int nrl, int nrh, int ncl, int nch) {
+    int i = 0, j = 0;
+    byte** gradient = bmatrix(nrl, nrh, ncl, nch);
+    for(i=nrl+1 ; i<nrh ; i++) {
+        for(j=ncl+1 ; j<nch ; j++) {
+            double squaredX = pow((double) gradX[i][j], 2);
+            double squaredY = pow((double) gradY[i][j], 2);
+            int pixel = sqrt(squaredX + squaredY);
+            gradient[i][j] = (byte) pixel;
+        }
+    }
+    return gradient;
+}
+
+byte** convolution(byte** orig, int mask[3][3], int nrl, int nrh, int ncl, int nch, int fraction) {
+    int i = 0, j = 0;
+    byte** transformed = bmatrix(nrl, nrh, ncl, nch);
+
+    for(i=nrl+1 ; i<nrh ; i++) {
+        for(j=ncl+1 ; j<nch ; j++) {
+            int pixel =
+                (orig[i-1][j-1]*mask[0][0] + orig[i-1][j]*mask[0][1] + orig[i-1][j+1]*mask[0][2]
+                + orig[i][j-1]*mask[1][0] + orig[i][j]*mask[1][1] + orig[i][j+1]*mask[1][2]
+                + orig[i+1][j-1]*mask[2][0] + orig[i+1][j]*mask[2][1] + orig[i+1][j+1]*mask[2][2])/fraction;
+            transformed[i][j] = (byte) abs(pixel);
+        }
+    }
+
+    return transformed;
+}
+
+int EP_from_image(byte** img, int nrl, int nrh, int ncl, int nch) {
+    int mask_gradX[3][3] = {{-1, 0, +1}, {-2, 0, +2}, {-1, 0, +1}};
+    int mask_gradY[3][3] = {{-1, -2, -1}, {0, 0, 0}, {+1, +2, +1}};
+
+    byte** gradX = convolution(img, mask_gradX, nrl, nrh, ncl, nch, 2);
+    byte** gradY = convolution(img, mask_gradY, nrl, nrh, ncl, nch, 2);
+    byte** gradient = gradientNorm(gradX, gradY, nrl, nrh, ncl, nch);
+    byte** gradient_bin = binarization(gradient, 70, nrl, nrh, ncl, nch);
+
+    int nb = edgePixels(gradient_bin, nrl, nrh, ncl, nch);
+
+    free_bmatrix(gradX, nrl, nrh, ncl, nch);
+    free_bmatrix(gradY, nrl, nrh, ncl, nch);
+    free_bmatrix(gradient, nrl, nrh, ncl, nch);
+    free_bmatrix(gradient_bin, nrl, nrh, ncl, nch);
+
+    return nb;
+}
+
 int main()
 {
 
@@ -93,6 +183,7 @@ int main()
     rgb8** IRGB;
 
     I = LoadPGM_bmatrix("cubesx3.pgm", &nrl, &nrh, &ncl, &nch);
+    printf("nombre de prixels du contours : %d \n", EP_from_image(I, nrl, nrh, ncl, nch));
 
     file = fopen ("data.csv", "w");
 
@@ -105,7 +196,8 @@ int main()
     histogramme(I, nrh, nch, histogram, file);
     fputc(';', file);
     
-    IRGB = LoadPPM_rgb8matrix("/home/amal/Documents/Master 2/Atelier Indexation d’images/archivePPMPGM/archive10ppm/arbre1.ppm", &nrl, &nrh, &ncl, &nch);
+    //IRGB = LoadPPM_rgb8matrix("/home/amal/Documents/Master 2/Atelier Indexation d’images/archivePPMPGM/archive10ppm/arbre1.ppm", &nrl, &nrh, &ncl, &nch);
+    IRGB = LoadPPM_rgb8matrix("archivePPMPGM/archive10ppm/arbre1.ppm", &nrl, &nrh, &ncl, &nch);
     histogrammeRGB(IRGB, nrh, nch, histogramR, histogramG, histogramB, file);
 
     printf("euclidienneDistance :%f \n",euclidienneDistance(histogramR, histogramR));
