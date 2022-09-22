447086ea8d8074da197724acd9ff82f656b146ec
imadhou <imadhou00@gmail.com>
2022-09-22 13:52:01 +0200

    histtttttttt

--- histogram.c
+++ histogram.c
@@ -1,95 +1,8 @@
-#ifndef DEF_HISTOGRAM
-#define DEF_HISTOGRAM
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
 #include "histogram.h"
-
-#include "def.h"
-#include "nrio.h"
-#include "nrarith.h"
-#include "nralloc.h"
-
-
-
-void histogramme(byte **img, long nrh, long nch, int *histogramme, FILE *file) {
-
-    //initialise l'histogramme
-    for (int i = 0; i < 256; i++) {
-        histogramme[i] = 0;
-    }
-
-    //classifie l'image suivant les différents niveaux de gris
-    for (int j = 0; j <= nrh; j++) {
-        for (int k = 0; k <= nch; k++) {
-            int index = img[j][k];
-            histogramme[index] = histogramme[index] + 1;
-        }
-    }
-
-    //sauvgarde de l'histogramme dans un fichier.csv
-    for(int i = 0; i < 256; i++)
-    {
-        // printf("%d : %d\n", i, histogramme[i]);
-        fprintf(file, "%d,", histogramme[i]);
-    }
-   
-}
-
-
-void histogrammeRGB(rgb8 **img, long nrh, long nch, int *histogrammeR, int * histogrammeG, int* histogrammeB, FILE *file) {
-
-    //initialise l'histogramme
-    for (int i = 0; i < 256; i++) {
-        histogrammeR[i] = 0;
-        histogrammeG[i] = 0;
-        histogrammeB[i] = 0;
-    }
-
-    //classifie l'image suivant les différents niveaux de gris
-    for (int j = 0; j <= nrh; j++) {
-        for (int k = 0; k <= nch; k++) {
-            int indexR = img[j][k].r;
-            int indexG = img[j][k].g;
-            int indexB = img[j][k].b;
-            histogrammeR[indexR] = histogrammeR[indexR] + 1;
-            histogrammeG[indexG] = histogrammeG[indexG] + 1;
-            histogrammeB[indexB] = histogrammeB[indexB] + 1;
-        }
-    }
- 
-    //sauvgarde de l'histogramme dans un fichier.csv
-    for(int i = 0; i < 256; i++)
-    {
-        // printf("%d : %d\n", i, histogrammeR[i]);
-        // printf("%d : %d\n", i, histogrammeG[i]);
-        // printf("%d : %d\n", i, histogrammeB[i]);
-
-        fprintf(file, "%d,", histogrammeR[i]);
-        fprintf(file, "%d,", histogrammeG[i]);
-        fprintf(file, "%d,", histogrammeB[i]);
-    }
-}
-
-double euclidienneDistance(int* histogramme1,int* histogramme2){
-    double distance=0.0;
-    for(int i=0;i < 256 ; i++){
-        distance+=(histogramme1[i]-histogramme2[i])*(histogramme1[i]-histogramme2[i]);
-    }
-    distance=sqrt(distance);
-    return distance;
-}
-
-double bhattacharyyaDistance(int* histogramme1,int* histogramme2){
-    double distance=0.0;
-    for(int i=0;i < 256 ; i++){
-        distance+=sqrt(histogramme1[i]*histogramme2[i]);
-    }
-    distance=-log(distance);
-    return distance;
-}
 
 byte** binarization(byte** orig, byte threshold, int nrl, int nrh, int ncl, int nch) {
     int i = 0, j = 0;
@@ -181,4 +94,145 @@ int edgePixelCounter(byte** img, int nrl, int nrh, int ncl, int nch) {
     return nb;
 }
 
-#endif
+
+int* histogramme(rgb8 **rgb8Img, long nrl, long nrh, long ncl, long nch) {
+
+    byte ** img = bmatrix(nrl, nrh, ncl, nch);
+    int *histogramme = (int *)malloc(sizeof(int)*256);
+
+    for(long i=0; i<=nrh; i++){
+        for(long j=0; j<=nch; j++){
+            img[i][j] =(byte)(0.3* rgb8Img[i][j].r + 0.59*rgb8Img[i][j].g + 0.11*rgb8Img[i][j].b);
+        }
+    }
+
+    //initialise l'histogramme
+    for (int i = 0; i < 256; i++) {
+        histogramme[i] = 0;
+    }
+
+    //classifie l'image suivant les différents niveaux de gris
+    for (int j = nrl; j <=nrh; j++) {
+        for (int k = ncl; k <=nch; k++) {
+            int index = img[j][k];
+            histogramme[index] = histogramme[index] + 1;
+        }
+    }
+
+    return histogramme;
+}
+
+double euclidienneDistance(int* histogramme1,int* histogramme2){
+    double distance=0.0;
+    for(int i=0;i < 256 ; i++){
+        distance+=(histogramme1[i]-histogramme2[i])*(histogramme1[i]-histogramme2[i]);
+    }
+    distance=sqrt(distance);
+    return distance;
+}
+
+double bhattacharyyaDistance(int* histogramme1,int* histogramme2){
+    double distance = 0.0;
+    for (int i = 0; i < 256; i++) {
+        distance += sqrt(histogramme1[i] * histogramme2[i]);
+    }
+    distance = -log(distance);
+    return distance;
+
+}
+
+void colorRate(rgb8 **img, long nrl, long nrh, long ncl, long nch, double *rater, double *rateg, double *rateb) {
+    (*rater) = 0.0;
+    (*rateg) = 0.0;
+    (*rateb) = 0.0;
+    for (int i = 0; i < nrh-nrl; i++) {
+        for (int j = 0; j < nch-ncl; j++) {
+            (*rater) += img[i][j].r;
+            (*rateg) += img[i][j].g;
+            (*rateb) += img[i][j].b;
+        }
+    }
+    int total=((*rater) + (*rateg) + (*rateb));
+    (*rater) = (*rater) / total;
+    (*rateg) = (*rateg) / total;
+    (*rateb) = (*rateb) / total;
+
+}
+
+double normGradientAverage(byte** img, long nrl, long nrh, long ncl, long nch){
+    double average = 0.0;
+    int horizontalFilter[FILTER_WIDTH][FILTER_HEIGHT] = {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+
+    int verticalFilter[FILTER_WIDTH][FILTER_HEIGHT] = {
+        {-1, -2, -1},
+        { 0,  0,  0},
+        { 1,  2,  1}
+    };
+
+    byte** imgn = bmatrix(nrl, nrh, ncl, nch);
+
+    
+    for (long h = nrl + 1; h < nrh; h++)
+    {
+        for (long w = ncl + 1; w < nch; w++)
+        {
+            // horizontal gradient
+            long totalH = 0l;
+            // top line
+            totalH += img[h - 1][w - 1] * horizontalFilter[0][0];
+            totalH += img[h - 1][w] * horizontalFilter[0][1];
+            totalH += img[h - 1][w + 1] * horizontalFilter[0][2];
+            // middle line
+            totalH += img[h][w - 1] * horizontalFilter[1][0];
+            totalH += img[h][w] * horizontalFilter[1][1];
+            totalH += img[h][w + 1] * horizontalFilter[1][2];
+            // bottom line
+            totalH += img[h + 1][w - 1] * horizontalFilter[2][0];
+            totalH += img[h + 1][w] * horizontalFilter[2][1];
+            totalH += img[h + 1][w + 1] * horizontalFilter[2][2];
+
+            totalH = abs(totalH) / 4l;
+
+            
+            // vertical gradient
+            long totalV = 0l;
+            // top line
+            totalV += img[h - 1][w - 1] * verticalFilter[0][0];
+            totalV += img[h - 1][w] * verticalFilter[0][1];
+            totalV += img[h - 1][w + 1] * verticalFilter[0][2];
+            // middle line
+            totalV += img[h][w - 1] * verticalFilter[1][0];
+            totalV += img[h][w] * verticalFilter[1][1];
+            totalV += img[h][w + 1] * verticalFilter[1][2];
+            // bottom line
+            totalV += img[h + 1][w - 1] * verticalFilter[2][0];
+            totalV += img[h + 1][w] * verticalFilter[2][1];
+            totalV += img[h + 1][w + 1] * verticalFilter[2][2];
+
+            totalV = abs(totalV) / 4l;
+            
+            
+            // gradient norm
+            byte norm = sqrt((totalH * totalH) + (totalV * totalV));
+            imgn[h][w] = norm;
+        }
+    }
+
+    for (long h = nrl + 1; h < nrh; h++)
+    {
+        for (long w = ncl + 1; w < nch; w++)
+        {
+            average += imgn[h][w];
+        }
+    }
+
+    average = average/((nrh + 1) * (nch + 1));
+
+    free_bmatrix(imgn, nrl, nrh, ncl, nch);
+
+    return average;
+}
--- histogram.h
+++ histogram.h
@@ -1,8 +1,10 @@
 #ifndef DEF_HISTOGRAM
 #define DEF_HISTOGRAM
 
-#include<stdio.h>
+#define FILTER_WIDTH 3
+#define FILTER_HEIGHT 3
 
+#include<stdio.h>
 #include "def.h"
 #include "nrio.h"
 #include "nrarith.h"
@@ -19,15 +21,15 @@ int vertical_gradient[3][3] = {
     {0,0,0},
     {1,2,1}
 };
-
-void histogramme(byte **img, long nrh, long nch, int *histogramme, FILE *file);
-void histogrammeRGB(rgb8 **img, long nrh, long nch, int *histogrammeR, int * histogrammeG, int* histogrammeB, FILE *file);
-double euclidienneDistance(int* histogramme1,int* histogramme2);
-double bhattacharyyaDistance(int* histogramme1,int* histogramme2);
 byte** binarization(byte** orig, byte threshold, int nrl, int nrh, int ncl, int nch);
 int pixelCounter(byte** gradient, int nrl, int nrh, int ncl, int nch);
 byte** gradientNorm(byte** gradX, byte**gradY, int nrl, int nrh, int ncl, int nch);
 byte** convolution(byte** orig, int mask[3][3], int nrl, int nrh, int ncl, int nch, int fraction);
 int edgePixelCounter(byte** img, int nrl, int nrh, int ncl, int nch);
+int* histogramme(rgb8 **rgb8Img, long nrl, long nrh, long ncl, long nch);
+double euclidienneDistance(int* histogramme1,int* histogramme2);
+double bhattacharyyaDistance(int* histogramme1,int* histogramme2);
+void colorRate(rgb8 **img, long nrl, long nrh, long ncl, long nch, double *rater, double *rateg, double *rateb);
+double normGradientAverage(byte** img, long nrl, long nrh, long ncl, long nch);
 
 #endif
